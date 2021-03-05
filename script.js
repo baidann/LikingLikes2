b0373b0f5f55b8e8eae2c64e168a567ba3e09e63
Danny Bai <baidanny091@gmail.com>
2021-03-04 21:52:47 -0500

    intial commit likes lvl2

---
+++ script.js
@@ -0,0 +1,21 @@
+var count1 = 9;
+var countElement1 = document.querySelector("#counter1")
+function add1(){
+    count1++;
+    countElement1.innerText = count1 + " likes(s)" ;
+}
+var count2 = 12;
+var countElement2 = document.querySelector("#counter2")
+function add2(){
+    count2++;
+    countElement2.innerText = count2 + " likes(s)" ;
+}
+var count3 = 9;
+var countElement3 = document.querySelector("#counter3")
+function add3(){
+    count3++;
+    countElement3.innerText = count3 + " likes(s)" ;
+}
+
+
+
